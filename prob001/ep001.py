801b0937efed2317a37451cd0e32285b2aa62787
Robert Stratton <r.a.stratton88@gmail.com>
2013-01-01 02:23:11 -0800

    edited problem 1

--- prob001/ep001.py
+++ prob001/ep001.py
@@ -1,25 +1,24 @@
+#!/usr/bin/python3
+
 """
 ==========================
-Project Euler - Problem 1:
+Project Euler - Problem 1
 ==========================
-Find the sum of all the multiples of 3 or 5 below 1000.
+Problem: Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-def solution1(lim):
+def solution1():
     """
     Return the sum of all the multiples of 3 or 5 below 'lim'
     """
-    return sum([n for n in xrange(1,lim) if n % 3 == 0 or n % 5 == 0])
+    return sum([n for n in range(1,1000) if n % 3 == 0 or n % 5 == 0])
 
-def solution2(lim):
+def solution2():
     """
     Return the sum of all the multiples of 3 or 5 below 'lim'
     """
     total = 0
-    for n in xrange(1,lim):
+    for n in range(1,1000):
         if n % 3 == 0 or n % 5 == 0:
             total += n
     return total
-
-x = input("Gimme")
-print(x)
