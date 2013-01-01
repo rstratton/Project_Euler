fb19ec18f389ba426b035342062a9a2f14037813
Robert Stratton <r.a.stratton88@gmail.com>
2013-01-01 03:01:22 -0800

    Added problem 1 in C

---
+++ prob001/prob001.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    int i, total = 0;
+    for (i = 1; i < 1000; ++i) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            total += i;
+        }
+    }
+    printf("%d\n", total);
+}
+
