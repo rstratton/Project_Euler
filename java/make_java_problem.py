cdc44c1eaece8d9561a8626ef73a3be95eab3de5
Robert Stratton <r.a.stratton88@gmail.com>
2014-03-19 22:44:47 -0700

    Improve problem stub creation script

--- java/make_java_problem.py
+++ java/make_java_problem.py
@@ -1,60 +1,67 @@
 #!/usr/bin/env python
+
+from __future__ import print_function
+from optparse import OptionParser
 import sys
 import os
 
 JAVA_TEMPLATE_NAME = "Problem.java.template"
 ANT_TEMPLATE_NAME = "build.xml.template"
 
+def parse_cmd_line_args():
+    parser = OptionParser()
+    parser.add_option("-b", "--build", action="store_true", default=False,
+                      help="Generate a build file for the problem")
+    parser.add_option("-c", "--code", action="store_true", default=False,
+                      help="Generate a code stub for the problem")
+    parser.add_option("-o", "--overwrite", action="store_true", default=False,
+                      help="Overwrite pre-existing build/code files")
+    return parser.parse_args()
+
 def verify_template_existance():
     for filename in (JAVA_TEMPLATE_NAME, ANT_TEMPLATE_NAME):
         if not os.path.isfile(filename):
-            print "Error: Required file '{0}' doesn't exist.".format(filename)
+            print("Error: Required file '{0}' doesn't exist.".format(filename),
+                  file=sys.stderr)
             sys.exit(1)
 
-def validate_environment():
-    """
-    Check that the proper template files and command line arguments are present
+def validate_args(args):
+    for arg in args:
+        try:
+            int(arg)
+        except ValueError as ex:
+            print("Error: '{0}' is not an integer.".format(arg), file=sys.stderr)
+            sys.exit(1)
 
-    """
-    verify_template_existance()
-    if len(sys.argv) != 2:
-        print_usage()
-        sys.exit(1)
-    try:
-        int(sys.argv[1])
-    except ValueError as ex:
-        print "Error: Received a non-integral problem number"
-        print_usage()
-        sys.exit(1)
+def create_code_stub(problem_num, overwrite=False):
+    filename = "{0}/Prob{0}.java".format(problem_num)
+    if overwrite or not os.path.exists(filename):
+        with open(JAVA_TEMPLATE_NAME) as f:
+            template = f.read()
+        with open(filename, "wb") as f:
+            f.write(template % (problem_num, problem_num))
 
-def create_class_stub(problem_num):
-    with open(JAVA_TEMPLATE_NAME) as f:
-        template = f.read()
-    with open("{0}/Prob{0}.java".format(problem_num), "wb") as f:
-        f.write(template % (problem_num, problem_num))
-
-def create_build_file(problem_num):
-    with open(ANT_TEMPLATE_NAME) as f:
-        template = f.read()
-    with open("{0}/build.xml".format(problem_num), "wb") as f:
-        f.write(template % (problem_num))
+def create_build_file(problem_num, overwrite=False):
+    filename = "{0}/build.xml".format(problem_num)
+    if overwrite or not os.path.exists(filename):
+        with open(ANT_TEMPLATE_NAME) as f:
+            template = f.read()
+        with open(filename, "wb") as f:
+            f.write(template % (problem_num))
 
 def create_problem_dir(problem_num):
     if not os.path.exists(problem_num):
         os.mkdir(problem_num)
 
-def print_usage():
-    usage_string = "Usage: {0} <problem_number>".format(sys.argv[0])
-    print usage_string
-
-
 if __name__ == "__main__":
 
-    validate_environment()
-    problem_num = sys.argv[1]
+    options, args = parse_cmd_line_args()
+    verify_template_existance()
+    validate_args(args)
 
-    create_problem_dir(problem_num)
-    create_class_stub(problem_num)
-    create_build_file(problem_num)
-
-
+    for problem_num in args:
+        create_problem_dir(problem_num)
+        if options.build:
+            create_build_file(problem_num, options.overwrite)
+        if options.code:
+            create_code_stub(problem_num, options.overwrite)
