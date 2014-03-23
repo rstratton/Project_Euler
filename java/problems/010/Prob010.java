da89646100578cae934313bf2bd053699bf77d34
Robert Stratton <r.a.stratton88@gmail.com>
2014-03-23 16:26:15 -0700

    Fix java problem 10

--- java/problems/010/Prob010.java
+++ java/problems/010/Prob010.java
@@ -1,26 +1,13 @@
+import java.util.ArrayList;
+
 public class Prob010 {
-	
-	public static void main(String[] args){
-		int size = 2000000;
-		long sum = 0l;
-		//long[] sieve = Erat.generateSieve(size);
-		for(int i = 0; i < size; ++i){
-			if(isPrime(i)){
-				sum += i;
-				System.out.println(i);
-			}
-		}
-		System.out.println(sum);
-	}
-	
-	public static boolean isPrime(int num){
-		if(num % 2 == 0 || num % 3 == 0)
-			return false;
-		for(int i = 6; i < num; i += 6){
-			if ((num % (i - 1)) == 0 || (num % (i + 1)) == 0){
-				return false;
-			}
-		}
-		return true;
-	}
+    public static void main(String[] args) {
+        PrimeSieve sieve = new PrimeSieve(2000001);
+        ArrayList<Integer> primesUnderTwoMillion = sieve.getPrimes();
+        long sum = 0l;
+        for (Integer prime : primesUnderTwoMillion) {
+            sum += prime;
+        }
+        System.out.println(sum);
+    }
 }
