d184317cab45831c419f0c1ab4fe52defb4383ec
Robert Stratton <r.a.stratton88@gmail.com>
2014-03-28 19:33:55 -0700

    Fix java problem 12

--- java/problems/012/Prob012.java
+++ java/problems/012/Prob012.java
@@ -1,45 +1,36 @@
-import java.lang.*;
-import java.math.*;
+import java.util.*;
+import java.lang.Integer;
 
 public class Prob012 {
-	public static final int MAX = 1000000;
-	public static int[] sieve = Erat.generateSieve(MAX);
-	
 	public static void main(String[] args){
-		int triNumber = 0;
-		for(int i = 1; i < MAX; ++i){
-			triNumber += i;
-			if(numFactors(triNumber) > 500)
-				System.out.println(triNumber);
-		}
+        for (Integer triangleNumber : new TriangleNumberList()) {
+            PrimeFactorization pf = new PrimeFactorization(triangleNumber);
+            if (pf.divisorCount() > 500) {
+                System.out.println(triangleNumber);
+                break;
+            }
+        }
 	}
-	
-	public static int numFactors(int num){
-		int[] primeFactorsCount = primeFactorsCount(num, null);
-		int primeCombos = 1;
-		for(int i = 0; i < primeFactorsCount.length; ++i){
-			primeCombos *= primeFactorsCount[i];
-		}
-		return primeCombos;
-	}
-	
-	public static int[] primeFactorsCount(int num, int[] primeFactorsCount){
-		if(primeFactorsCount == null){
-			primeFactorsCount = new int[sieve.length];
-			for(int i = 0; i < primeFactorsCount.length; ++i){ //Fill occurrences array with 1's
-				primeFactorsCount[i] = 1;
-			}
-		}
-		if(num == 1){
-			return primeFactorsCount;
-		}
-		for(int i = 0; i < num && i < sieve.length; ++i){
-			if(num % sieve[i] == 0){
-				++(primeFactorsCount[i]);
-				primeFactorsCount(num / sieve[i], primeFactorsCount);
-				break;
-			}
-		}
-		return primeFactorsCount;		
-	}
+
+    static class TriangleNumberList implements Iterable<Integer> {
+        @Override
+        public Iterator<Integer> iterator() {
+            return new Iterator<Integer>() {
+                private int lastTriangleNumber = 0;
+                private int nextAddition = 1;
+
+                @Override
+                public Integer next() {
+                    lastTriangleNumber += nextAddition++;
+                    return new Integer(lastTriangleNumber);
+                }
+
+                @Override
+                public boolean hasNext() { return true; }
+
+                @Override
+                public void remove() {}
+            };
+        }
+    }
 }
