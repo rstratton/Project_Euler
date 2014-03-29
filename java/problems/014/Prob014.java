3b108578c22fbfc42e51203ed5b9b0d616bb5619
Robert Stratton <r.a.stratton88@gmail.com>
2014-03-28 23:24:09 -0700

    Change java problem 14 to be more efficient

--- java/problems/014/Prob014.java
+++ java/problems/014/Prob014.java
@@ -1,38 +1,42 @@
-import java.util.Scanner;
-import java.math.BigInteger;
+import java.util.*;
 
 public class Prob014 {	
-	public static void main(String[] args){
-		System.out.println(findLongestLength(1000000));
-	}
-	
-	public static long collatzLength(BigInteger startNum){
-		long pathLength = 1;
-		if(startNum.compareTo(new BigInteger("1")) == 0 || startNum.compareTo(new BigInteger("0")) == 0)
-			return pathLength;
-		if(startNum.mod(new BigInteger("2")).compareTo(new BigInteger("0")) == 0){
-			pathLength += collatzLength(startNum.divide(new BigInteger("2")));
-			return pathLength;
-		}
-		else{
-			pathLength += collatzLength(startNum.multiply(new BigInteger("3")).add(new BigInteger("1")));
-			return pathLength;
-		}
-	}
-	
-	public static long findLongestLength(long max){
-		long longestLength = 0;
-		long number = 0;
-		long length = 0;
-		for(int i = 0; i <= max; ++i){
-			length = collatzLength(new BigInteger(new Integer(i).toString()));
-			if(length >= longestLength){
-				number = i;
-				longestLength = length;
-			}
-		}
-		return number;
-	}
-}
+    public static Map<Long, Integer> chainLengthCache =
+            new HashMap<Long, Integer>(1000000);
 
-		
+	public static void main(String[] args){
+        int longestChainLength = 0;
+        int longestChainStart = -1;
+        for (int i = 1; i < 1000000; ++i) {
+            int chainLength = collatzChainLength(i);
+            if (chainLength > longestChainLength) {
+                longestChainLength = chainLength;
+                longestChainStart = i;
+            }
+        }
+        System.out.println(longestChainStart);
+    }
+
+    // Get the length of the Collatz chain starting at "num"
+    public static int collatzChainLength(int start) {
+        int length = 1;
+        for (long current = start; current != 1l; current = next(current)) {
+            if (chainLengthCache.containsKey(current)) {
+                length += chainLengthCache.get(current) - 1;
+                break;
+            }
+            length++;
+        }
+        chainLengthCache.put((long) start, length);
+        return length;
+    }
+
+    // Return the next number in the Collatz chain
+    public static long next(long num) {
+        if (num % 2 == 0) {
+            return num / 2;
+        } else {
+            return 3 * num + 1;
+        }
+    }
+}
