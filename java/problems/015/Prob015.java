843565dd921926ceaaf0f3d911a7d3c2a98bf4f9
Robert Stratton <r.a.stratton88@gmail.com>
2014-03-28 23:55:46 -0700

    Solve problem 15 much more efficiently

--- java/problems/015/Prob015.java
+++ java/problems/015/Prob015.java
@@ -1,109 +1,31 @@
 public class Prob015 {
-	static final int GRID_SIZE = 21;
-	static boolean[][] grid = new boolean[GRID_SIZE][GRID_SIZE];	//'true' indicates that node hasn't been travelled.
-	static long pathCount = 0;
-	static int x = 0;
-	static int y = 0;
-	
-	public static void main(String[] args){
-		initializeGrid();
-		traverseGrid(0, 0);
-		System.out.println(pathCount);
-	}
-	
-	public static void traverseGrid(int xStart, int yStart){
-		if(x == 0 && y == 0)
-			grid[y][x] = false;
-		if(x == GRID_SIZE - 1 && y == GRID_SIZE - 1){
-			++pathCount;
-			return;
-		}
-		if(checkDown()){
-			++y;
-			grid[y][x] = false;
-			traverseGrid(xStart, yStart + 1);
-			grid[y][x] = true;
-			--y;
-		}
-		/*
-		if(checkUp()){
-			--y;
-			grid[y][x] = false;
-			traverseGrid(xStart, yStart - 1);
-			grid[y][x] = true;
-			++y;
-		}
-		*/
-		if(checkRight()){
-			++x;
-			grid[y][x] = false;
-			traverseGrid(xStart + 1, yStart);
-			grid[y][x] = true;
-			--x;
-		}
-		/*
-		if(checkLeft()){
-			--x;
-			grid[y][x] = false;
-			traverseGrid(xStart - 1, yStart);
-			grid[y][x] = true;
-			++x;
-		}
-		*/
-		return;
-	}
-			
-			
-	
-	public static void initializeGrid(){
-		for(int i = 0; i < GRID_SIZE; ++i){
-			for(int j = 0; j < GRID_SIZE; ++j){
-				grid[i][j] = true;
-			}
-		}
-	}
-	
-	public static boolean hasMove(){
-		if(checkDown() || checkUp() || checkLeft() || checkRight())
-			return true;
-		else
-			return false;
-	}
-	
-	public static boolean checkDown(){
-		if(y == GRID_SIZE - 1)
-			return false;
-		if(grid[y + 1][x] == true)
-			return true;
-		else
-			return false;
-	}
-	
-	public static boolean checkUp(){
-		if(y == 0)
-			return false;
-		if(grid[y-1][x] == true)
-			return true;
-		else
-			return false;
-	}
-	
-	public static boolean checkRight(){
-		if(x == GRID_SIZE - 1)
-			return false;
-		if(grid[y][x + 1] == true)
-			return true;
-		else
-			return false;
-	}
-	
-	public static boolean checkLeft(){
-		if(x == 0)
-			return false;
-		if(grid[y][x - 1] == true)
-			return true;
-		else
-			return false;
-	}
+    // NOTE: This problem can also be solved by evaluating "40 choose 20";
+    // you have to take 40 discrete steps to get from the start point of the
+    // grid to the end.  Half of those steps will be a downward movement, so
+    // choosing 20 of the original 40 to be downward steps determines the entire
+    // route, hence 40 choose 20
+
+    public static void main(String[] args) {
+        int gridSize = 20;
+
+        // routeCount[i][j] represents the number of routes from position
+        // (i, j) on the grid to the destination point (0, 0)
+        long[][] routeCount = new long[gridSize + 1][gridSize + 1];
+
+        // Initialize the lookup table with base cases (i.e. when there is only
+        // one possible route)
+        for (int i = 0; i < routeCount.length; ++i) {
+            routeCount[i][0] = 1;
+            routeCount[0][i] = 1;
+        }
+
+        // Populate the routeCount table using dynamic programming
+        for (int i = 1; i < routeCount.length; ++i) {
+            for (int j = 1; j < routeCount[i].length; ++j) {
+                routeCount[i][j] = routeCount[i][j-1] + routeCount[i-1][j];
+            }
+        }
+
+        System.out.println(routeCount[gridSize][gridSize]);
+    }
 }
-		
