827d2080cbf873feddfbdf79e230beb9d6d0344a
Robert Stratton <r.a.stratton88@gmail.com>
2014-05-17 16:21:40 -0700

    Refactor problem 022

--- python/problems/022/022.py
+++ python/problems/022/022.py
@@ -1,15 +1,22 @@
-import string
-namesfile = open("names.txt")
-namesstring = namesfile.read()
-nameslist = namesstring.split(",")
-for i in range(len(nameslist)):
-    nameslist[i] = nameslist[i].split("\"")[1].lower()
-nameslist.sort()
-namescoresum = 0
-for i in range(len(nameslist)):
-    charlist = list(nameslist[i])
-    namesubscore = 0
-    for j in range(len(charlist)):
-        namesubscore += ord(charlist[j]) - ord('a') + 1
-    namescoresum += namesubscore * (i + 1)
-print namescoresum
+#!/usr/bin/env python2
+
+def get_names_list():
+    with open("names.txt") as f:
+        quoted_names = f.read().split(",")
+        return [name.strip('"') for name in quoted_names]
+
+def name_score(index, name):
+    return name_value(name) * (index + 1)
+
+def name_value(name):
+    return sum([char_value(char) for char in name])
+
+def char_value(char):
+    return ord(char.upper()) - ord("A") + 1
+
+def main():
+    sorted_names = sorted(get_names_list())
+    print sum([name_score(i, name) for (i, name) in enumerate(sorted_names)])
+
+if __name__ == "__main__":
+    main()
